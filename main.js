9b174562f8beb7568a46ef63646b23a0fc31fcdd
YI-JYUN HE <jobs.steve54@gmail.com>
2023-11-29 12:37:32 +0800

    修改
    修改程式

--- main.js
+++ main.js
@@ -30,3 +30,122 @@ let data = [
       "rate": 7
     }
   ];
+
+// 覆蓋套票、篩選器
+const ticketCard = document.querySelector(".ticketCard-area");
+const regionSearch = document.querySelector(".regionSearch");
+const searchResultText = document.querySelector("#searchResult-text");
+
+// 查無資料頁面
+const cantFindarea = document.querySelector(".cantFind-area");
+// 必填隱藏
+const circle = document.querySelector(".fa-exclamation-circle");
+
+// 新增套票
+const ticketName = document.querySelector("#ticketName");
+const ticketImgUrl = document.querySelector("#ticketImgUrl");
+const ticketRegion = document.querySelector("#ticketRegion");
+const ticketPrice = document.querySelector("#ticketPrice");
+const ticketNum = document.querySelector("#ticketNum");
+const ticketRate = document.querySelector("#ticketRate");
+const ticketDescription = document.querySelector("#ticketDescription");
+const addTicketButton = document.querySelector(".addTicket-btn");
+const ticketForm = document.querySelector(".addTicket-form");
+// 表單
+
+  function init(data) {
+    let str = "";
+    data.forEach(function (element) {
+      let li = `<li class="ticketCard">
+      <div class="ticketCard-img">
+          <a href="#">
+              <img src="${element.imgUrl} alt="">
+          </a>
+          <div class="ticketCard-region">${element.area}
+          </div>
+          <div class="ticketCard-rank">${element.rate}
+          </div>
+      </div>
+      <div class="ticketCard-content">
+          <div>
+              <h3>
+                  <a href="#" class="ticketCard-name">${element.name}
+                  </a>
+              </h3>
+              <p class="ticketCard-description">${element.description}
+              </p>
+          </div>
+          <div class="ticketCard-info">
+              <p class ="ticketCard-num"> 
+                  <span><i class="fas fa-exclamation-circle"></i>
+                  </span>
+                  <span>剩下最後<span id="ticketCard-num"> ${element.group} </span> 組
+              </p>
+              <p class="ticketCard-price">
+                  TWD <span id="ticketCard-price">$${element.price}</span>
+              </p>
+          </div>
+      </div>
+  </li>`;
+      str += li;
+    });
+    ticketCard.innerHTML = str;
+    searchResultText.textContent = `本次搜尋共 ${data.length} 筆資料`;
+  }
+  //覆蓋套票
+  
+  init(data);
+  
+  //篩選地區
+  regionSearch.addEventListener("change", addTicket);
+  
+  function addTicket() {
+    let newData = [];
+    data.forEach(function (element) {
+      if (regionSearch.value === element.area) {
+        cantFindarea.style.display = "block";
+        newData.push(element);
+        rendering(newData);
+      } else if (regionSearch.value === "") {
+        init(data);
+      }
+    });
+  }
+  
+  //新增套票
+  
+  addTicketButton.addEventListener("click", function (e) {
+    if (ticketName.value === "") {
+      return;
+    } else if (ticketImgUrl.value === "") {
+      return;
+    } else if (ticketRegion.value === "") {
+      return;
+    } else if (ticketPrice.value === "") {
+      return;
+    } else if (ticketNum.value === "") {
+      return;
+    } else if (ticketRate.value === "") {
+      return;
+    } else if (ticketDescription.value === "") {
+      return;
+    } else {
+      alert("新增成功！");
+    }
+  
+    let obj = {};
+    obj.name = ticketName.value;
+    obj.imgUrl = ticketImgUrl.value;
+    obj.area = ticketRegion.value;
+    obj.price = ticketPrice.value;
+    obj.group = ticketNum.value;
+    obj.rate = ticketRate.value;
+    obj.description = ticketDescription.value;
+    data.push(obj);
+  
+    init(data);
+    ticketForm.reset();
+    ticketCard.scrollIntoView({ behavior: "auto", block: "end" });
+    regionSearch.value = "";
+  });
+  
